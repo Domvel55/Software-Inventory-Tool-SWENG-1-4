f1c947ea5dff4cc3982fb5e246d146fbe83cfdfd
Chad36390 <91159991+Chad36390@users.noreply.github.com>
2021-10-05 08:04:55 -0400

    Add files via upload
    Added a main window, with the name of the tool at the top.

---
+++ main.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import *
+
+root = tk.Tk()
+root.title('Software Inventory Tool')
+
+mainWindow = tk.Label(
+                background = "#2a3439",
+                width = 192,
+                height = 64
+
+)
+
+"""
+button = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
+"""
+mainWindow.pack()
+#button.pack()
+
+
+
+main=mainloop()

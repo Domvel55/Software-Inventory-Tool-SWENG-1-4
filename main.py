14fe4835c962a616d0bbf91ca0c2858262812c40
DominickVelardo <velard13@students.rowan.edu>
2021-10-05 12:09:52 -0400

    Added create_window() function

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import tkinter as tk
 from tkinter import *
 
-root = tk.Tk()
-root.title('Software Inventory Tool')
 
-mainWindow = tk.Label(
+def create_window():
+    root = tk.Tk()
+    root.title('Software Inventory Tool')
+
+    mainWindow = tk.Label(
                 background = "#2a3439",
                 width = 192,
                 height = 64
+    )
 
-)
-
-"""
-button = tk.Button(
-    text="Click me!",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow",
-)
-"""
-mainWindow.pack()
-#button.pack()
-
+    """
+    button = tk.Button(
+        text="Click me!",
+        width=25,
+        height=5,
+        bg="blue",
+        fg="yellow",
+    )
+    """
+    mainWindow.pack()
+    #button.pack()
 
 
-main=mainloop()
+if __name__ == '__main__':
+    create_window()
+    main=mainloop()

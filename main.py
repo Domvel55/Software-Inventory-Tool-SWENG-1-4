5ae846f90f46ae762f75d74d915b8b1ae7fd01ac
Razzberry7 <68514613+Razzberry7@users.noreply.github.com>
2021-10-07 08:46:07 -0400

    Changed a bit of Chad's code, and added comments to most blocks.
    Provided the framework for the way buttons link to pages and how pages are constructed.
    
    Created a help button and a help page.

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import tkinter as tk
 from tkinter import *
+from tkinter_custom_button import TkinterCustomButton
+
+
+class MainWindow():
+    def init(self, root):
+        #root.geometry("600x600")
+        #root.minsize(750, 600)
+        root.configure(background="#2a3439")
+
 
 
 def create_window():
     root = tk.Tk()
-    root.configure(bg="#2A3439")
 
+    # Initialize all windows
+    MainWindow().init(root)
+    HelpPage().init(root)
+
+    # Window background color
+    root.configure(background="#2a3439")
+
+    # Not sure really
+    #frame = tk.Frame(root)
+    #frame.pack()
+
+    # Scaling UI to user's screen
     app_width = 1000
     app_height = 600
-
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
-
     x = (screen_width / 2) - (app_width / 2)
     y = (screen_height / 2) - (app_height / 2)
-
     root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 
     # remove title bar
     root.overrideredirect(True)
 
+    #
     def move_app(e):
         root.geometry(f'+{e.x_root}+{e.y_root}')
 
+    #
     def quitter(e):
         root.quit()
         root.destroy()
 
+    #
     def minimizer(e):
         root.overrideredirect(0)
         root.iconify()
@@ -34,25 +54,53 @@ def create_window():
     # Create New Title Bar
     title_bar = Frame(root, bg="#1F262A", relief="raised", bd=1)
     title_bar.pack(fill=X)
+
     # 'Binding the title bar
     title_bar.bind("<B1-Motion>", move_app)
 
+    # Navigation Buttons
+    #This one is a freebie Vince (sorry/you're welcome)
+    #home_button = tk.Button(title_bar, text='Home', command=lambda: MainWindow().init(root))
+    #home_button.pack(side=LEFT, padx=5)
+
+    #Results Button here
+
+    #Settings Button here
+
+    #Future version
+    #help_button = TkinterCustomButton(title_bar, text='Help', command=lambda: HelpPage().init(root))
+
+    help_button = tk.Button(title_bar, text='Help', command=lambda: HelpPage().init(root))
+    help_button.pack(side=LEFT, padx=5)
+
+
+
+# Will need to fix where the label is placed (there is not center align ugh)
     # Create Title Text
-    title_lable = Label(title_bar, text="Software Inventory Tool", bg="#1F262A", fg="white")
-    title_lable.pack(side=LEFT, pady=4, padx=425)
+    title_label = Label(title_bar, text="Software Inventory Tool", bg="#1F262A", fg="white")
+    title_label.pack(side=LEFT, pady=4, padx=100)
 
     # Create close button
-    close_lable = Label(title_bar, text="X  ", bg="#1f262A", fg="white", relief="raised", bd=0)
-    close_lable.pack(side=RIGHT, pady=4)
-    close_lable.bind("<Button-1>", quitter)
+    close_label = Label(title_bar, text="X  ", bg="#1f262A", fg="white", relief="raised", bd=0)
+    close_label.pack(side=RIGHT, pady=4)
+    close_label.bind("<Button-1>", quitter)
 
     # Create Minimize button
-    minimize_lable = Label(title_bar, text="---", bg="#1f262A", fg="white", relief="raised", bd=0)
-    minimize_lable.pack(side=RIGHT, pady=4)
-    minimize_lable.bind("<Button-1>", minimizer)
+    minimize_label = Label(title_bar, text="---", bg="#1f262A", fg="white", relief="raised", bd=0)
+    minimize_label.pack(side=RIGHT, pady=4)
+    minimize_label.bind("<Button-1>", minimizer)
 
     root.mainloop()
 
 
+class HelpPage():
+
+    def init(self, root):
+        # root.geometry("600x600")
+        # root.minsize(750, 600)
+        root.configure(background="#fff000")
+
+
 if __name__ == '__main__':
     create_window()
+    main=mainloop()

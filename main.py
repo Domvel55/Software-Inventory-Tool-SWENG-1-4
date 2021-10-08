3b01901edf51343f857c3de7acceaa1d0d63d0c1
abrusc93 <90268822+abrusc93@users.noreply.github.com>
2021-10-08 18:09:46 -0400

    Added/changed a few things
    Made only the main window be initialized at launch.
    Added Express Scan Button.
    Added sample settings to settings page.

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ class MainWindow():
 def create_window():
     root = tk.Tk()
 
-    # Initialize all windows
+    # Initialize Main Window
     MainWindow().init(root)
-    HelpPage().init(root)
-    SettingsPage().init(root)
 
     # Window background color
     root.configure(background="#2a3439")
@@ -60,19 +58,47 @@ def create_window():
     title_bar.bind("<B1-Motion>", move_app)
 
     # Navigation Buttons
-    home_button = tk.Button(title_bar, text='Home', command=lambda: MainWindow().init(root))
+    home_button = TkinterCustomButton(master=title_bar, bg_color=None,
+                                          fg_color="#1F262A",
+                                          hover_color="#2a3439",
+                                          text_font="Bold, 14",
+                                          text="Home",
+                                          text_color="white",
+                                          corner_radius=0,
+                                          width=75,
+                                          height=40,
+                                          hover=True,
+                                          command=lambda: MainWindow().init(root))
     home_button.pack(side=LEFT, padx=5)
 
     # Results Button here
 
     # Create Settings Button
-    settingsButton = tk.Button(title_bar, text="Settings", command=lambda: SettingsPage().init(root))
-    settingsButton.pack(side=LEFT, padx=5)
+    settings_button = TkinterCustomButton(master=title_bar, bg_color=None,
+                                      fg_color="#1F262A",
+                                      hover_color="#2a3439",
+                                      text_font="Bold, 14",
+                                      text="Settings",
+                                      text_color="white",
+                                      corner_radius=0,
+                                      width=75,
+                                      height=40,
+                                      hover=True,
+                                      command=lambda: SettingsPage().init(root))
+    settings_button.pack(side=LEFT, padx=5)
 
-    # Future version
-    # help_button = TkinterCustomButton(title_bar, text='Help', command=lambda: HelpPage().init(root))
-
-    help_button = tk.Button(title_bar, text='Help', command=lambda: HelpPage().init(root))
+    # Create Help Button
+    help_button = TkinterCustomButton(master=title_bar, bg_color=None,
+                                      fg_color="#1F262A",
+                                      hover_color="#2a3439",
+                                      text_font="Bold, 14",
+                                      text="Help",
+                                      text_color="white",
+                                      corner_radius=0,
+                                      width=75,
+                                      height=40,
+                                      hover=True,
+                                      command=lambda: HelpPage().init(root))
     help_button.pack(side=LEFT, padx=5)
 
     # Will need to fix where the label is placed (there is not center align ugh)
@@ -90,7 +116,18 @@ def create_window():
     minimize_label.pack(side=RIGHT, pady=4)
     minimize_label.bind("<Button-1>", minimizer)
 
+    # Create Express Scan Button
+    expressScanButton = tk.Button(root, text="Express Scan")
+    expressScanButton.place(relx=0.65, rely=0.85)
+
     root.mainloop()
+
+class ResultsPage():
+
+    def init(self, root):
+        # root.geometry("600x600")
+        # root.minsize(750, 600)
+        root.configure(background="#fff000")
 
 
 class HelpPage():
@@ -104,26 +141,60 @@ class HelpPage():
 class SettingsPage():
 
     def init(self, root):
-        settingsFrame = ttk.Frame(root)
-        settingsFrame.place(relx=0.5, rely=0.5, anchor='center')
-        settingsFrame.config(height=300, width=500)
-        settingsFrame.config(relief=RIDGE)
+        settings_frame = ttk.Frame(root)
+        settings_frame.place(relx=0.5, rely=0.5, anchor='center')
+        settings_frame.config(height=500, width=700)
+        settings_frame.config(relief=RIDGE)
 
-        settingsPageLabel = ttk.Label(settingsFrame, text='Settings Page')
-        settingsPageLabel.place(relx=0.5, rely=0.15, anchor='center')
+        settings_page_label = ttk.Label(settings_frame, text='Settings Page')
+        settings_page_label.place(relx=0.5, rely=0.15, anchor='center')
 
-        setOptionsFrame = ttk.Frame(settingsFrame)
-        setOptionsFrame.place(relx=0.5, rely=0.5, anchor='center')
-        setOptionsFrame.config(height=300, width=500)
-        setOptionsFrame.config(relief=RIDGE)
-        setOptionsFrame.config(padding=(30, 15))
+        set_options_frame = ttk.Frame(settings_frame)
+        set_options_frame.place(relx=0.5, rely=0.5, anchor='center')
+        set_options_frame.config(height=300, width=500)
+        set_options_frame.config(relief=RIDGE)
+        set_options_frame.config(padding=(30, 15))
 
-        set1label = ttk.Label(setOptionsFrame, text='Setting 1')
-        set1label.grid(row=0, column=0)
-        set2label = ttk.Label(setOptionsFrame, text='Setting 2')
-        set2label.grid(row=1, column=0)
-        set3label = ttk.Label(setOptionsFrame, text='Setting 3')
-        set3label.grid(row=2, column=0)
+        set_1_label = ttk.Label(set_options_frame, text='Text size')
+        set_1_label.grid(row=0, column=0, padx=50, pady=30)
+        decrease_txt_size_button = ttk.Button(set_options_frame, text='-')
+        decrease_txt_size_button.grid(row=0, column=1)
+        txt_size_entry = ttk.Entry(set_options_frame, width=5)
+        txt_size_entry.grid(row=0, column=2)
+        txt_size_entry.insert(0, '12')
+        increase_txt_size_button = ttk.Button(set_options_frame, text='+')
+        increase_txt_size_button.grid(row=0, column=3)
+
+        set_2_label = ttk.Label(set_options_frame, text='Sort scan results...')
+        set_2_label.grid(row=1, column=0, padx=50)
+        sort_order = StringVar()
+        set_2_button_1 = ttk.Radiobutton(set_options_frame, text='By severity', variable = sort_order, value = 'severity')
+        set_2_button_1.grid(row=1, column=2)
+        set_2_button_2 = ttk.Radiobutton(set_options_frame, text='In order discovered', variable = sort_order, value = 'discovered')
+        set_2_button_2.grid(row=2, column=2)
+
+        set_3_label = ttk.Label(set_options_frame, text='Choose directories to ignore:')
+        set_3_label.grid(row=3, column=0, padx=50, pady=30)
+        set_3_button = ttk.Button(set_options_frame, text='Browse...')
+        set_3_button.grid(row=3, column=2)
+
+        set_4_label = ttk.Label(set_options_frame, text='When scan finishes...')
+        set_4_label.grid(row=4, column=0, padx=50)
+        after_scan = StringVar()
+        set_4_button_1 = ttk.Radiobutton(set_options_frame, text='Do Nothing', variable = after_scan, value = 'nothing')
+        set_4_button_1.grid(row=4, column=2)
+        set_4_button_2 = ttk.Radiobutton(set_options_frame, text='Close the program', variable = after_scan, value = 'close')
+        set_4_button_2.grid(row=5, column=2)
+        set_4_button_3 = ttk.Radiobutton(set_options_frame, text='Shut down computer', variable = after_scan, value = 'shut_down')
+        set_4_button_3.grid(row=6, column=2)
+
+        set_5_label = ttk.Label(set_options_frame, text='Reset Default Settings')
+        set_5_label.grid(row=7, column=0, padx=50, pady=30)
+        set_5_button = ttk.Button(set_options_frame, text='Reset')
+        set_5_button.grid(row=7, column=2)
+
+        apply_button = ttk.Button(settings_frame, text = 'Apply')
+        apply_button.place(relx=0.5, rely=0.95, anchor='center')
 
 
 if __name__ == '__main__':
